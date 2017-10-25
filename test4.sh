99929f7efc87bc07d23b5d54e405b80ba96e1a59
jguoae <32601561+jguoae@users.noreply.github.com>
2017-10-25 16:34:49 -0400

    Update test4.sh

--- test4.sh
+++ test4.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1 
 #SBATCH -p RM
-#SBATCH --ntasks-per-node 28
+#SBATCH --ntasks-per-node 7
 #SBATCH -t 00:05:00
 # echo commands to stdout•set -x
 # run OpenMP program
-export OMP_NUM_THREADS=28
-./587p4bfs.out
+export OMP_NUM_THREADS=7
+./587p4.out
