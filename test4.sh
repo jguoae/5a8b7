d852457ef02dde7fc438195a3fef44e3a7a4230e
jguoae <32601561+jguoae@users.noreply.github.com>
2017-10-25 15:17:57 -0400

    Add files via upload

---
+++ test4.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p RM
+#SBATCH --ntasks-per-node 28
+#SBATCH -t 00:05:00
+# echo commands to stdout•set -x
+# run OpenMP program
+export OMP_NUM_THREADS=28
+./587p4.out
