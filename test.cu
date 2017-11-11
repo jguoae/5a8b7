d37ed57540d56e20a2ae4e1a1460d0451bea97a8
jguoae <32601561+jguoae@users.noreply.github.com>
2017-11-11 16:33:52 -0500

    Add files via upload

--- test.cu
+++ test.cu
@@ -1,33 +1,37 @@
 
+#define N 512
+
 __global__ void add(int *a, int *b, int *c) {
 
-*c = *a + *b;
+c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
 
 }
 
+
 int main(void) {
 
-int a, b, c; // host copies of a, b, c
+int *a, *b, *c; // host copies of a, b, c
 int *d_a, *d_b, *d_c; // device copies of a, b, c
-int size = sizeof(int);
+int size = N * sizeof(int);
 
-// Allocate space for device copies of a, b, c
+// Alloc space for device copies of a, b, c
 cudaMalloc((void **)&d_a, size);
 cudaMalloc((void **)&d_b, size);
 cudaMalloc((void **)&d_c, size);
-// Setup input values
-a = 2;
-b = 7;
+// Alloc space for host copies of a, b, c and setup input values
+a = (int *)malloc(size); random_ints(a, N);
+b = (int *)malloc(size); random_ints(b, N);
+c = (int *)malloc(size);
 
-
-// Copy inputs to device
-cudaMemcpy(d_a, &a, size, cudaMemcpyHostToDevice);
-cudaMemcpy(d_b, &b, size, cudaMemcpyHostToDevice);
-// Launch add() kernel on GPU
-add<<<1,1>>>(d_a, d_b, d_c);
+cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+// Launch add() kernel on GPU with N blocks
+add<<<N,1>>>(d_a, d_b, d_c);
 // Copy result back to host
-cudaMemcpy(&c, d_c, size, cudaMemcpyDeviceToHost);
+cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
 // Cleanup
+free(a); free(b); free(c);
 cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
 return 0;
+
 }
