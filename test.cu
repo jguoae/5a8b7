4ea0ab213330049aea07298b45957eefae5f5495
jguoae <32601561+jguoae@users.noreply.github.com>
2017-11-11 18:49:58 -0500

    Add files via upload

--- test.cu
+++ test.cu
@@ -1,4 +1,7 @@
 #include <math.h>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
 
 
 #define N 512
@@ -15,6 +18,10 @@ int main(void) {
 int a[N], b[N], c[N]; // host copies of a, b, c
 int *d_a, *d_b, *d_c; // device copies of a, b, c
 int size = N * sizeof(int);
+for(int i=0;i<N;i++){
+  a[i]=1;
+  b[i]=1;
+}
 
 // Alloc space for device copies of a, b, c
 cudaMalloc((void **)&d_a, size);
@@ -31,6 +38,7 @@ add<<<N,1>>>(d_a, d_b, d_c);
 cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
 // Cleanup
 cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+cout<<c[0]<<endl;
 return 0;
 
 }
